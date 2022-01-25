ad2ff2c90e4f36aa6e1195b10ee8436f8582687d
DiegoTeam <diego.team@ctl.com.ar>
2022-01-25 17:55:01 -0300

    Create chatDemorado.js

---
+++ chatDemorado.js
@@ -0,0 +1,29 @@
+/*exports.handler = async function(context, variables) {
+
+var config = {
+    method: 'post',
+    url: 'https://flows.messagebird.com/flows/20c71c5a-3cd4-464b-bc2f-cb68a6a106d3/invoke',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    data: data
+};
+
+var axios = require('axios');
+    var data = {
+        'id': variables.id,
+        'ticketUrl': variables.ticketUrl,
+        'eventType': variables.eventType,
+    }
+
+    axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+    var response = await axios(config)
+
+}*/
